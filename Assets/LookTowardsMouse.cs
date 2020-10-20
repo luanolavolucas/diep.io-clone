dd53e81c10733607f31b6a32997fbcc0788c43be
luanlucas <30471817+luanolavolucas@users.noreply.github.com>
2020-10-20 01:36:44 -0300

    Added Cinemachine

--- Assets/LookTowardsMouse.cs
+++ Assets/LookTowardsMouse.cs
@@ -15,16 +15,8 @@ public class LookTowardsMouse : MonoBehaviour
     void Update()
     {
         Vector3 target = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, transform.position.z));
-        //transform.rotation.SetLookRotation(difference, Vector3.forward);
-        //transform.LookAt(target, Vector3.back);
-        //Vector3.fo
-        
         Vector3 difference = target - transform.position;
-        //Vector3 difference = transform.position - target;
         float rotationZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(0.0f, 0.0f, rotationZ);
-
-        //float angle = Vector2.Angle(transform.right,  new Vector2(difference.x,difference.y));
-        //transform.RotateAround(pivot.transform.position, Vector3.back, angle);
     }
 }
